aa3c43ddfa473df64343f408861ba3c94ee487f4
Stephen Pak <stephenjpak@gmail.com>
2022-04-07 21:10:51 -0700

    4-7-22-9:10

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,8 +2,32 @@
 //   console.log('function was called');
 // };
 
+// var getUserRepos = function () {
+//   fetch('https://api.github.com/users/octocat/repos');
+// };
+
+// var getUserRepos = function () {
+//   var response = fetch('https://api.github.com/users/octocat/repos');
+//   console.log(response);
+// };
+
+//deming asynchronous communication showing that second console log is processed first
+// var getUserRepos = function () {
+//   fetch('https://api.github.com/users/octocat/repos').then(function (response) {
+//     //console.log('inside', response);
+//     console.log(response);
+//   });
+// };
+
+// getUserRepos();
+
+//to make the above json response more legible
 var getUserRepos = function () {
-  fetch('https://api.github.com/users/octocat/repos');
+  fetch('https://api.github.com/users/octocat/repos').then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
 
 getUserRepos();
