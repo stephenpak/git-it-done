93834a509c4a9fbb020eafdf804894d1f7e95fc4
Stephen Pak <stephenjpak@gmail.com>
2022-04-07 21:22:32 -0700

    4-7-22-9:22

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -21,9 +21,25 @@
 
 // getUserRepos();
 
-//to make the above json response more legible
-var getUserRepos = function () {
-  fetch('https://api.github.com/users/octocat/repos').then(function (response) {
+//to make the above response more legible with json syntax
+// var getUserRepos = function () {
+//   fetch('https://api.github.com/users/octocat/repos').then(function (response) {
+//     response.json().then(function (data) {
+//       console.log(data);
+//     });
+//   });
+// };
+
+// getUserRepos();
+
+//upto now, API requests were tested to make sure it works before incorporating to function
+//now we can re write the function that everything was verified
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
     response.json().then(function (data) {
       console.log(data);
     });
